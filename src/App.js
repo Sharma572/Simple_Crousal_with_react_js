5aca21d964e3171f5421626a2bf477f5bc596117
Raunak Sharma <98183441+Sharma572@users.noreply.github.com>
2022-10-09 23:24:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Testimonials from "./Testimonals";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Testimonials</h1>
+        <Testimonials />
+      </header>
+    </div>
+  );
+}
+
+export default App;
